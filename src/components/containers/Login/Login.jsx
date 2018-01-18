2dfb074565764ab9497d6e5469763ad846c1a7ca
Charlie Fan <chen.fan1991@outlook.com>
2018-01-18 15:54:16 -0500

    init styles library

--- src/components/containers/Login/Login.jsx
+++ src/components/containers/Login/Login.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import styles from './Login.scss'
 
 export default class Home extends React.Component {
+    state = {
+        formData: {
+            email: '',
+            password: ''
+        }
+    }
+
     render() {
         return (
-            <div>
+            <div className={styles.login_view}>
                 <h1>Login</h1>
+                <button type="button" className="btn btn-primary">Primary</button>
             </div>
         )
     }
